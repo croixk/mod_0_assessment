ba91c2daa695ff33f58af0069667c26979d76082
Croix Kimmel <croixkimmel@gmail.com>
2021-09-24 09:17:52 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = 'Harry Potter'
+is_entertaining = true
+movie = 'Blades of Glory'
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ['quinoa', 'beans', 'spinach']
+favorite_foods.shift
+puts movie.length()
