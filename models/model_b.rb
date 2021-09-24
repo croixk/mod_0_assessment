6c04f7231390b40d6f8dce729f9a0d2bdb97f8d1
Croix Kimmel <croixkimmel@gmail.com>
2021-09-24 09:36:53 -0600

    Add model_b.rb with developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,4 @@
+developer = {'name' => 'jill', 'favorite_language' => 'ruby',
+  'years_of_experience' => 2, 'languages_known' => 4,
+  'backend_specialist' => true, 'frontend_specialist' => false,
+  'favorite_drinks' => ['coffee', 'tea', 'beer'], 'favorite_hobbies' => ['hiking', 'camping', 'cards']}
