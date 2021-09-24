13b1deb716f9458aa7d06c894c3b5e2cd6d03f22
Croix Kimmel <croixkimmel@gmail.com>
2021-09-24 09:30:18 -0600

    Add model_a.rb with dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,4 @@
+dinosaur = {'name' => 'fred', 'species' => 'stegosaurus', 'age' => 25,
+  'weight_in_pounds' => 10000, 'is_big' => true, 'is_old' => true,
+  'favorite_foods' => ['grass', 'tree leaves', 'bushes'], 'friends' => ['bob',
+    'sue', 'joe']}
